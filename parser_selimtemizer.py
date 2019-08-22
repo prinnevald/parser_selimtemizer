c09a8d09e6b5147d96beb2b4454fe7d4f741e7af
prinnevald <prinnyprinnevald@gmail.com>
2019-08-23 01:48:57 +0600

    adding zipping and refactoring

--- parser_selimtemizer.py
+++ parser_selimtemizer.py
@@ -1,31 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+import shutil
 import time
 import os
 
+path = "files"
 source = "http://selimtemizer.com/csci235/"
-
-val = requests.session()
-Data = val.get(source).text
-bs = BeautifulSoup(Data, 'lxml')
-elements = bs.find_all('a')
+info = requests.session().get(source).text
+elements = BeautifulSoup(info, 'lxml').find_all('a')
 
 
-def parseSource():
-	for each in elements:
-		
-		link = source + each.get('href')
-		name = (each.get_text()).replace(" ", "_")
-		r = requests.get(link)
-
-		if not os.path.exists("files"):
-			os.makedirs("files")
-
-		with open("files/" + name, 'wb') as f:
-			f.write(r.content)
-		
-		time.sleep(1)
+def parseFiles():
+    for each in elements:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with open(path + '/' + each.get_text().replace(" ", "_"), 'wb') as f:
+            f.write(requests.get(source + each.get('href')).content)
+        time.sleep(1)
 
 
-parseSource()
+def zipFiles():
+    parseFiles()
+    where = os.path.join(os.getcwd(), path)
+    shutil.make_archive(os.path.join(os.getcwd(), 'archive'), 'zip', where)
+    shutil.rmtree(os.path.join(os.getcwd(), path))
+
+
+print("Compress the files into a .zip folder? Y or N:")
+if(input() == 'Y' or 'y'):
+    zipFiles()
+else:
+    parseFiles()
 print("Parsing Complete!")
