92fc86d406da9ce598dd336bffd4edb43de22fb3
prinnevald <prinnyprinnevald@gmail.com>
2019-08-22 13:15:48 +0600

    only requests, no urllib

--- parser_selimtemizer.py
+++ parser_selimtemizer.py
@@ -1,24 +1,31 @@
 from bs4 import BeautifulSoup
-import urllib.request
 import requests
 import time
+import os
 
 source = "http://selimtemizer.com/csci235/"
 
-req = urllib.request.Request(source)
-resp = urllib.request.urlopen(req)
-Data = resp.read()
-bs = BeautifulSoup(Data, 'html.parser')
+val = requests.session()
+Data = val.get(source).text
+bs = BeautifulSoup(Data, 'lxml')
+elements = bs.find_all('a')
 
 
 def parseSource():
-	for each in bs.find_all('a'):
+	for each in elements:
+		
 		link = source + each.get('href')
 		name = (each.get_text()).replace(" ", "_")
 		r = requests.get(link)
-		with open(name, 'wb') as f:
+
+		if not os.path.exists("files"):
+			os.makedirs("files")
+
+		with open("files/" + name, 'wb') as f:
 			f.write(r.content)
+		
 		time.sleep(1)
 
 
 parseSource()
+print("Parsing Complete!")
